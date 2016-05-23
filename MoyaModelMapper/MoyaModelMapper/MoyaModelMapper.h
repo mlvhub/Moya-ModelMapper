07b26d39f5f10cf33aabc03d306be40064fc9818
Miguel Lopez V <mlopezva@akamai.com>
2016-05-22 21:19:31 -0600

    Create an Xcode project
    - Add a Cartfile with the respective dependencies

---
+++ MoyaModelMapper/MoyaModelMapper/MoyaModelMapper.h
@@ -0,0 +1,19 @@
+//
+//  MoyaModelMapper.h
+//  MoyaModelMapper
+//
+//  Created by Lopez Valenciano, Miguel on 5/22/16.
+//  Copyright © 2016 MoyaModelMapper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MoyaModelMapper.
+FOUNDATION_EXPORT double MoyaModelMapperVersionNumber;
+
+//! Project version string for MoyaModelMapper.
+FOUNDATION_EXPORT const unsigned char MoyaModelMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoyaModelMapper/PublicHeader.h>
+
+
---
+++ MoyaModelMapper/RxMoyaModelMapper/RxMoyaModelMapper.h
@@ -0,0 +1,19 @@
+//
+//  RxMoyaModelMapper.h
+//  RxMoyaModelMapper
+//
+//  Created by Lopez Valenciano, Miguel on 5/22/16.
+//  Copyright © 2016 MoyaModelMapper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxMoyaModelMapper.
+FOUNDATION_EXPORT double RxMoyaModelMapperVersionNumber;
+
+//! Project version string for RxMoyaModelMapper.
+FOUNDATION_EXPORT const unsigned char RxMoyaModelMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxMoyaModelMapper/PublicHeader.h>
+
+
